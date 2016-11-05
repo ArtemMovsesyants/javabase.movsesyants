fa9b6de25b400c01faf29d608fdb8804962a3fcc
Артем <@192.168.2.106>
2016-11-05 17:59:53 +0200

    Checking

--- src/slow/sad/java/firstTry/Main.java
+++ src/slow/sad/java/firstTry/Main.java
@@ -3,8 +3,7 @@ package slow.sad.java.firstTry;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("\t\tHello World");
-		System.out.println("\t\tHello World");
+		System.out.println("\t\tHello");
 	}
 
 }
